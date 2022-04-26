742a2405e555e6b60ba0f6f2d1bbf46636316cfa
Binoy Das Gupta <62295509+Binoy02@users.noreply.github.com>
2022-04-26 21:53:12 +0600

    Add files via upload

---
+++ String_Input(1).cpp
@@ -0,0 +1,19 @@
+/*
+  Binoy Das Gupta
+  Write a C++ program to read and print string(1).
+  26-04-2022
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	//input one string and printing it.
+	char s[100];
+	cout<<"Enter a string : ";
+	cin>>s;
+	
+	cout<<s<<endl;
+	return 0;
+}
---
+++ String_Input(2).cpp
@@ -0,0 +1,18 @@
+/*
+  Binoy Das Gupta
+  Write a C++ program to read and print string.(2)
+  26-04-2022
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	//Input more than one word and print it in a single line.
+	char s[100];
+	cout<<"Enter string : ";
+	cin.get(s,100);
+	cout<<"Welcome "<<s<<endl;
+	return 0;
+}
